b85edb30fd2b7bfbaf00e6ebb68adbeb7f33f38e
Wesley Leach <leachw9001@gmail.com>
2016-03-29 06:08:03 -0700

    Pushing compiled bits
    My verison will crash if there isn't a wizard.dll on disk. It would
    create it for some reaosn. Its just easier to mutilate the git ignore
    file.

---
+++ Intermediate/Build/Win64/UE4Editor/Development/Wizards/Wizards.h
@@ -0,0 +1,5 @@
+#ifndef __AUTO_WIZARDS_H__
+#define __AUTO_WIZARDS_H__
+//Last Write: 3/19/2016 11:30:00 PM +00:00
+#include "I:\Projects\wizards\Source\Wizards\Wizards.h"
+#endif//__AUTO_WIZARDS_H__
